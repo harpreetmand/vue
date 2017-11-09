be845401c2c75a5ef0d282190222f4a0ec3bfafb
Harpreet Mand <harpreet@limeadvertising.com>
2017-11-09 11:28:22 -0500

    first commit and working

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>trade or view Portfoilio</h1>
+      <p>your Funds: {{ funds }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+      funds() {
+          return this.$store.getters.funds;
+      }
+  }
+}
+</script>
---
+++ src/components/data/data.js
@@ -0,0 +1,5 @@
+export default [
+    { id: 1, name: 'BMW', price: 100 },
+    { id: 2, name: 'goog', price: 100 },
+    { id: 3, name: 'app', price: 100 }
+]
